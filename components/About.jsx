d41394361ecf85d9797141c21fd00e7ef4537307
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-07-10 16:38:40 +0700

    file updates

---
+++ components/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about-container" id="about-scroll">
+      <div className="about-inner-container">
+        <h2>About</h2>
+        <p>
+          With a strong background in marketing, I've been passionate about
+          training and empowering aspiring marketers for the past 8 years.
+        </p>
+        <br></br>
+        <p>
+          I have designed and delivered comprehensive digital marketing
+          frameworks and courses for enthusiastic apprentices and students
+          throughout the UK and have empowered newcomers to the digital world
+          and helped them maximise the exciting opportunities available.
+        </p>
+        <br></br>
+        <p>
+          My interest in web development began during my university days, where
+          I built blogs using WordPress.com. Since then, I have worked with
+          various Content Management Systems such as Squarespace, WIX, and
+          Webflow.
+        </p>
+        <br></br>
+        <p>
+          This sparked my curiosity about the technologies behind those CMS
+          platforms and how they functioned. While running a Limited company in
+          digital recruitment, I witnessed the immense power and potential of
+          web development and the tech industry as a whole.
+        </p>
+        <br></br>
+        <p>
+          Inspired by this realisation, I embarked on a self-taught journey to
+          become a front-end web developer. In addition to expanding my
+          front-end skills, I actively engage with a growing community on
+          LinkedIn, emphasising the significance of digital portfolios.
+        </p>
+        <br></br>
+        <p>
+          I offered my services to aspiring marketers and junior front-end
+          developers, helping them create impactful digital portfolios that can
+          enhance their job prospects. Today? I can't wait to start a new career
+          in tech. Thanks for reading.
+        </p>
+      </div>
+    </div>
+  );
+}
